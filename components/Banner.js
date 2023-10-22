b1194c14c536948d5657a12871ef1a6d9ed70fe7
shahbaz <quazi.shahbaz95@gmail.com>
2023-10-23 01:29:20 +0530

    banner fix

--- components/Banner.js
+++ components/Banner.js
@@ -26,10 +26,10 @@ function Banner() {
       <div className="relative">
         <div className="w-screen h-screen">
           <div className = { hide ? `${styles.hideElement}`: `${styles.showElement}`} >
-            <h1 className="absolute bottom-0 left-1/2 font-sans font-extrabold text-2xl text-white z-10 -translate-x-20 -translate-y-10 min-[320px]:text-lg sm:text-2xl">
+            <h1 className="absolute bottom-14 left-1/2 font-sans font-extrabold text-2xl text-white z-10 -translate-x-20 -translate-y-10 min-[320px]:text-lg sm:text-2xl">
               Unveil your Growth
             </h1>
-            <FaArrowDown className="animate-bounce absolute bottom-0 left-1/2 text-2xl invert z-10" />
+            <FaArrowDown className="animate-bounce absolute bottom-14 left-1/2 text-2xl invert z-10" />
           </div>
         </div>
         <video
