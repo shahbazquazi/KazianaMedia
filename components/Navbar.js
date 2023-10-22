a59cc81f406f7e2892f801a29f7dcba67776b3d1
shahbaz <quazi.shahbaz95@gmail.com>
2023-10-23 00:34:09 +0530

    icon fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,11 +16,11 @@ function Navbar() {
 
   return (
     <div>
-      <nav className="fixed bg-transparent text-metallicGoldS w-screen h-24 z-30 max-[820px]:bg-black">
+      <nav className="fixed bg-transparent text-metallicGoldS w-screen h-24 z-30 max-[820px]:bg-black h-16">
         <div className="grid grid-cols-2">
-          <div className="flex justify-start items-end ml-5 h-20">
+          <div className="flex justify-start items-end ml-5 h-20  max-[640px]:ml-0">
             <Link href="/">
-            <img src="/logo.svg" alt="Traffkey" className="h-10 m-5" />
+            <img src="/logo.svg" alt="KazianaMedia" className="h-10 m-5"/>
             </Link>
           </div>
           <div className="flex justify-end items-end h-18 mr-16 mb-3 max-[640px]:mr-5 h-16">
