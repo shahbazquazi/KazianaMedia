4cf9bb6d869d46fbeeeb4021ddeec88e457aa613
shahbaz <quazi.shahbaz95@gmail.com>
2023-11-15 02:16:34 +0530

    email changed

--- components/Emailus.js
+++ components/Emailus.js
@@ -10,7 +10,7 @@ function Emailus() {
         transition={{ duration: 1 }}
         className=" text-white absolute left-1/4 top-2/4 text-center font-sans font-bold md:text-4xl w-1/2  z-10 lg:text-4xl sm:text-3xl max-[640px]:text-3xl"
       >
-        Mail us at : <br/><span className="text-[#d4af37]">kazianamedia<br/>@gmail.com</span>
+        Mail us at : <br/><span className="text-[#d4af37]">contact@<br/>kazianamedia.com</span>
       </motion.h1>
       <img className="w-screen h-screen opacity-30" src="/images/emailus.jpg" alt="Email Us" />
     </div>
